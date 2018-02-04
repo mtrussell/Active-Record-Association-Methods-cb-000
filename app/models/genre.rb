e8c2d00fdad3cdf10613ac932a529d85e6085081
Matt Trussell <smtrussell@gmail.com>
2018-02-04 01:20:44 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,5 +14,9 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_names = []
+    self.artists.each do |artist|
+      artist_names << artist.name
+    end
   end
 end
