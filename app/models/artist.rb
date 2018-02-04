8c80e5c80f4044b52a461b7da781bcc2969bd1e3
Matt Trussell <smtrussell@gmail.com>
2018-02-04 01:13:23 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,16 +9,16 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
+  end
+
+  def genre_count
+    #return the number of genres associated with the artist
     genres = []
     self.songs.each do |x|
       genres << x.genre
     end
     genres.uniq!
     genres.count
-  end
-
-  def genre_count
-    #return the number of genres associated with the artist
-    arr = self.songs
   end
 end
